2738de30a5d5f0e26b88989089a9e0d952ada65a
Unknown <alapizco29@gmail.com>
2019-03-10 21:24:04 -0700

    Funcionalidad en consola de numeros arabicos

--- ConsoleNumerosRomanos/ConsoleNumerosRomanos/Program.cs
+++ ConsoleNumerosRomanos/ConsoleNumerosRomanos/Program.cs
@@ -10,42 +10,82 @@ namespace ConsoleNumerosRomanos
     {
         static void Main(string[] args)
         {
+            int opcion = -1;
             int numeroInicial = 0;
             int numeroFinal = 0;
             bool continuar = true;
-            string saltoLinea = string.Empty;
+            bool continuarMenu = true;
+            string saltoLinea = "\n";
             string resp;
-            CTiposNumeros romano;
+            CTiposNumeros numero;
             do
             {
-                Console.WriteLine(saltoLinea + "Números romanos por rango.....");
+                Console.WriteLine("Menú de opciones:");
+                Console.WriteLine("Opción 1: Convertir números Arabicos a Romanos");
+                Console.WriteLine("Opción 2: Convertir números Romanos a Arabicos");
                 Console.WriteLine("Para salir ingrese 0 (cero) o solo deje vacio y presione enter");
-                Console.Write("Ingrese el número inicial: ");
+                Console.Write("Seleccione la opción deseada: ");
                 resp = Console.ReadLine();
-                numeroInicial = string.IsNullOrWhiteSpace(resp) ? 0 : int.Parse(resp);
-
-                continuar = ValidaRespuesta(numeroInicial);
-
-                if (continuar)
+                opcion = string.IsNullOrWhiteSpace(resp) ? 0 : int.Parse(resp);
+                do
                 {
-                    Console.Write("Ingrese el número final: ");
-                    resp = Console.ReadLine();
-                    numeroFinal = string.IsNullOrWhiteSpace(resp) ? 0 : int.Parse(resp);
-                    continuar = ValidaRespuesta(numeroInicial);
-
-                    if (continuar)
+                    if (opcion == 0)
                     {
-                        romano = new CTiposNumeros();
-                        for (int i = numeroInicial; i <= numeroFinal; i++)
+                        continuarMenu = false;
+                        continuar = false;
+                    }
+                    else if (opcion == 2)
+                    {
+                        Console.WriteLine(saltoLinea + "Números romanos por rango.....");
+                        Console.WriteLine("Para salir ingrese 0 (cero) o solo deje vacio y presione enter");
+                        Console.Write("Ingrese el número inicial: ");
+                        resp = Console.ReadLine();
+                        numeroInicial = string.IsNullOrWhiteSpace(resp) ? 0 : int.Parse(resp);
+
+                        continuar = ValidaRespuesta(numeroInicial);
+
+                        if (continuar)
                         {
-                            int num = i;
-                            Console.WriteLine("Número = {0}\tRomano = {1}",i, romano.ConvierteARomano(ref num));
+                            Console.Write("Ingrese el número final: ");
+                            resp = Console.ReadLine();
+                            numeroFinal = string.IsNullOrWhiteSpace(resp) ? 0 : int.Parse(resp);
+                            continuar = ValidaRespuesta(numeroInicial);
+
+                            if (continuar)
+                            {
+                                numero = new CTiposNumeros();
+                                for (int i = numeroInicial; i <= numeroFinal; i++)
+                                {
+                                    int num = i;
+                                    Console.WriteLine("Número = {0}\tRomano = {1}", i, numero.ConvierteARomano(ref num));
+                                }
+                            }
                         }
                     }
-                }
-                saltoLinea = "\n";
+                    else if (opcion == 1)
+                    {
+                        Console.WriteLine(saltoLinea + "Números Romanos a Arabicos");
+                        Console.WriteLine("Para salir ingrese 0 (cero) o solo deje vacio y presione enter");
+                        Console.Write("Ingrese el número romano a convertir: ");
+                        resp = Console.ReadLine();
+                        string romanoAConvertir = string.IsNullOrWhiteSpace(resp) ? "0" : resp;
 
-            } while (continuar);
+                        if (romanoAConvertir.Equals("0"))
+                        {
+                            continuar = false;
+                        }else
+                        {
+                            numero = new CTiposNumeros();
+                            Console.WriteLine(saltoLinea + "> {0}\t{1}",romanoAConvertir, numero.ConvierteAArabico(romanoAConvertir).ToString());
+                        }
+
+                    }else
+                    {
+                        Console.WriteLine("Error");
+                    }
+
+                } while (continuar);
+            } while (continuarMenu);
 
             Console.WriteLine("Pulse cualquier tecla para salir...");
             Console.ReadKey();
